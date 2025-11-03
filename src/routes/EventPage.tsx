bea083930d8204c96d974dfbd9404c9d376e79e1
Michael Swetnam-Burland <mjswetnamburla@wm.edu>
2025-11-03 18:28:59 -0500

    Naive supabase implementation

--- src/routes/EventPage.tsx
+++ src/routes/EventPage.tsx
@@ -3,9 +3,48 @@ import ErrorComponent from "../components/ErrorCompnent";
 import Cookies from "../assets/cookies.jpeg";
 import EventBadge from "../components/EventBadge";
 
-function Success() {
-    const badges = ["October 28, 2025", "7-9 PM", "Hardy Hall", "W&M Students"];
-    const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia nisl rutrum augue varius, ac porta augue porta. Morbi venenatis pretium urna, ac pellentesque odio placerat at. Curabitur nisl nisi, ultricies in volutpat ut, porttitor eu est. Sed vitae commodo enim, at posuere felis. Sed sed turpis lectus. Vestibulum lacinia volutpat felis convallis iaculis. Fusce interdum, neque vitae euismod facilisis, velit erat molestie ex, sed imperdiet augue tellus sit amet arcu. Nullam felis mi, ultrices in cursus eu, faucibus et metus. In hac habitasse platea dictumst. ";
+import { createClient, PostgrestError } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+const PROJECT_URL = "https://okoywtixurlcfnkraeob.supabase.co";
+const PUBLIC_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rb3l3dGl4dXJsY2Zua3JhZW9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMDExNjcsImV4cCI6MjA3Nzc3NzE2N30.Vtyh85w1_WYOjLeGbXNo35JwEMOG7E0Bo1tdFFr4vYw";
+
+function Success({ id }: { id: number }) {
+    const [event, setEvent] = useState<{
+        accessability: "AllStudents" | "ClubMembers";
+        description: string;
+        end: string;
+        id: number;
+        location: string;
+        name: string;
+        start: string;
+    } | null>(null);
+    const [error, setError] = useState<PostgrestError | null>(null);
+
+    useEffect(() => {
+        const getData = async () => {
+            const client = createClient(PROJECT_URL, PUBLIC_API_KEY);
+
+            const { data, error } = await client.from("Events").select("*").eq('id', id).single();
+            setError(error);
+            setEvent(data);
+        }
+        getData();
+    }, [id])
+
+
+    if (error) {
+        return <ErrorComponent message="Could not find this event" technical={`Database: ${error.code} - ${error.message}`}/>
+    }
+
+    if (event === null) {
+        return <div>Loading...</div>
+    }
+
+    const badges: string[] = [];
+    badges.push(event.start);
+    badges.push(event.location);
+    badges.push(event.accessability);
 
     return <div className="flex-1 flex flex-col justify-start items-center gap-2">
         <h2 className="font-bold text-3xl p-2 text-orange-700">This page is in construction</h2> 
@@ -20,14 +59,14 @@ function Success() {
                 backgroundSize: "cover"
             }}>
                 <div className="mb-60 p-2 rounded-xl bg-white shadow-sm">
-                    <span className={"font-bold text-2xl"} style={{ color: "black" }}>Spooky Cookie Decoration</span>
+                    <span className={"font-bold text-2xl"} style={{ color: "black" }}>{event.name}</span>
                 </div>
                 <div className="flex flex-wrap gap-2">
-                    { badges.map(t => <EventBadge text={t} />) }
+                    { badges.map((t, i) => <EventBadge key={i} text={t} />) }
                 </div>
             </div>
             <div className="p-6 text-gray-800 leading-relaxed text-sm md:text-base">
-                {description}
+                {event.description}
             </div>
             { /* DO NOT USE THIS SHIT EVERYTHING IS BAD AND UGLY */}
             <div className="pl-6 text-gray-800 leading-relatex text-sm md:text-base">Probably some more information down here</div>
@@ -49,5 +88,5 @@ export default function EventPage() {
         return <ErrorComponent message="Could not find the event you are looking for." technical={`Expected :id property from router with type number. Given: '${id}' which is NaN.`} />
     }
     
-    return <Success />
+    return <Success id={parsedId}/> 
 }
