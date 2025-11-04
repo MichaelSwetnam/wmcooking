d6db53ff6171ca72235cf2d70cfc7ee9c7770cf4
Michael Swetnam-Burland <mjswetnamburla@wm.edu>
2025-11-04 14:35:37 -0500

    Added screen for mobile devices on /events

--- src/routes/Events.tsx
+++ src/routes/Events.tsx
@@ -5,6 +5,7 @@ import { PostgrestError } from "@supabase/supabase-js";
 import ErrorComponent from "../components/ErrorComponent";
 import LoadingComponent from "../components/LoadingComponent";
 import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 function DateBase({ day, info, active }: { day: Date, info: EventRecord[], active: boolean }) {
     const nav = useNavigate();
@@ -61,10 +62,17 @@ function DateBuilder({ day, info }: { day: Date, info: EventRecord[] }) {
 }
 
 export default function Events() {
+    const nav = useNavigate();
     const [data, setData] = useState<EventRecord[] | null>(null);
     const [error, setError] = useState<PostgrestError | null>(null);
 
     useEffect(() => {
+        if (window.innerWidth < 768) {
+            // Will start an infinite load. This is because I don't want to query DB when this page won't even render on mobile no matter what.
+            setData(null);
+            setError(null);
+        }
+
         const fx = async () => {
             const { data, error } = await Database.getEventsInCurrentMonth();
             setData(data);
@@ -72,6 +80,18 @@ export default function Events() {
         };
         fx();
     }, []);
+
+    if (window.innerWidth < 768) {
+        return <div className="flex-1 flex flex-col justify-center items-center gap-3">
+            <h2 className="pt-5 text-3xl md:text-4xl font-extrabold text-orange-700 text-center">This page is not available on mobile devices yet. Sorry!</h2>
+            <div className="flex flex-col md:flex-row gap-1">
+                <p className="text-gray-700">If you have any questions or concerns, please email us at </p>
+                <a href="mailto:cooking@wm.edu" className="text-gray-700 underline hover:text-blue-600 transition duration-150">cooking@wm.edu</a>
+            </div>
+            
+            <Button onClick={() => nav("/")}>Return to Main Page</Button>
+        </div>
+    }
 
     if (error) {
         return <ErrorComponent message="Could not find this event" technical={`Database: ${error.code} - ${error.message}`} />;
