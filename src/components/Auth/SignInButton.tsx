b40ca0fa558310c30ae33c92aa3b971567a3ef2c
Michael Swetnam-Burland <mjswetnamburla@wm.edu>
2025-11-12 11:16:58 -0500

    Remove google icon from sign in button

--- src/components/Auth/SignInButton.tsx
+++ src/components/Auth/SignInButton.tsx
@@ -9,10 +9,6 @@ export default function Component() {
         onClick={signIn}
         className="cursor-pointer flex gap-2 items-center justify-center bg-blue-100 text-blue-900 font-medium px-3 py-1 rounded-full hover:shadow-md transition-shadow"
     >
-        <img
-            className="w-8 h-8 rounded-full object-cover"
-            src="https://www.gstatic.com/marketing-cms/assets/images/d5/dc/cfe9ce8b4425b410b49b7f2dd3f3/g.webp=s48-fcrop64=1,00000000ffffffff-rw"
-        ></img>
         <span>Sign in</span>
     </button>
 }
