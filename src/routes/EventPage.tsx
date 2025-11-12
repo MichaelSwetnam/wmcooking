32ee94a1e7127aa733c225a4082173e7867fdc1e
Michael Swetnam-Burland <mjswetnamburla@wm.edu>
2025-11-12 17:00:38 -0500

    Immediately broke - done for today

--- src/routes/EventPage.tsx
+++ src/routes/EventPage.tsx
@@ -1,17 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ErrorComponent from "../components/Event/ErrorComponent";
 import  Database, { type EventRecord } from "../lib/Database";
 import LoadingComponent from "../components/Utility/LoadingComponent";
 import { useEffect, useState } from "react";
 import type { PostgrestError } from "@supabase/supabase-js";
 import EventPage from "../components/Event/EventPage";
+import OAuth from "../lib/OAuth";
+import Button from "../components/Utility/Button";
 
 export default function Page() {
     const { id } = useParams();
-    
-    
     const [event, setEvent] = useState<EventRecord | null>(null);
     const [error, setError] = useState<PostgrestError | null>(null);
+    const [isPrivileged, setPrivilege] = useState(false);
+    const nav = useNavigate();
 
     useEffect(() => {
         const getData = async () => {
@@ -24,7 +26,13 @@ export default function Page() {
             setError(error);
             setEvent(data);
         }
+        const getAuth = async () => {
+            const isAuth = await OAuth.isPrivileged();
+            if (isAuth === true)
+                setPrivilege(true);
+        }
         getData();
+        getAuth();
     }, [id])
 
     if (!id) {
@@ -45,8 +53,11 @@ export default function Page() {
     }
 
 
-    return <div className="flex flex-col justify-start items-center gap-2 h-full">
+    return <div className="flex-1 flex flex-col justify-start items-center gap-2 h-full">
         <h2 className="pt-5 text-3xl md:text-4xl font-extrabold text-orange-700 text-center">Event Details:</h2>
         <EventPage event={event}/> 
+        {isPrivileged &&
+            <Button onClick={() => nav(`/events/${id}/edit`)}>Edit</Button>
+        }
     </div>;
 }
