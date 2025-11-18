8603f9227088dcb545c70df7d29708b10051812e
Michael Swetnam-Burland <mjswetnamburla@wm.edu>
2025-11-18 11:52:26 -0500

    Emergency push

--- src/lib/Supabase.ts
+++ src/lib/Supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const PROJECT_URL = "https://okoywtixurlcfnkraeob.supabase.co";
-const PUBLIC_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rb3l3dGl4dXJsY2Zua3JhZW9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMDExNjcsImV4cCI6MjA3Nzc3NzE2N30.Vtyh85w1_WYOjLeGbXNo35JwEMOG7E0Bo1tdFFr4vYw";
+const PROJECT_URL = "https://ysqrkscfqagmvjdglxcb.supabase.co";
+const PUBLIC_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzcXJrc2NmcWFnbXZqZGdseGNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NjM2ODUsImV4cCI6MjA3OTAzOTY4NX0.1BVCqXI-zNsdgOUKZfP53E8Z3AC0Z0vPLFjlqF4CJuo";
 
 export const Supabase = createClient(PROJECT_URL, PUBLIC_API_KEY);
