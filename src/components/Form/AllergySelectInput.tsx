c686dc4ebfe4192f429c98ca99063a1183a4b68f
Michael Swetnam-Burland <mjswetnamburla@wm.edu>
2026-01-04 18:08:10 -0500

    Better, more user-friendly  selection

--- src/components/Form/AllergySelectInput.tsx
+++ src/components/Form/AllergySelectInput.tsx
@@ -5,6 +5,21 @@ import Database from "../../lib/Database/Database";
 import DBError from "../../lib/Database/DBError";
 import ErrorComponent from "../Event/ErrorComponent";
 
+function SelectComponent({ id, displayText, startValue, callback }: { id: string, displayText: string, startValue: boolean, callback: (id: string, value: boolean) => void }) {
+    const [value, setValue] = useState(startValue);
+
+    return <button 
+        onClick={e => {
+            e.preventDefault();
+            setValue(!value);
+            callback(id, !value);
+        }}
+        className={`py-1 px-10 rounded-sm cursor-pointer hover:shadow-md transition-shadow ` + (value ? "bg-blue-600 text-white" : "bg-gray-300 text-black")}
+    >
+        {displayText}
+    </button>
+}
+
 export default function AccessabilityInput({ event }: { event: EventWrapper }) {
     const [ allergyOptions, setOptions ] = useState<string[] | null>(null);
     const [ selectedAllergies, setSelected ] = useState<string[] | null>(null);
@@ -25,33 +40,36 @@ export default function AccessabilityInput({ event }: { event: EventWrapper }) {
     if (error) return <ErrorComponent message={error.message} />
     if (!allergyOptions || !selectedAllergies) return <LoadingComponent />
 
-    function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
-        const selected = [];
-        for (let i = 0; i < e.target.selectedOptions.length; i++) {
-            const item = e.target.selectedOptions.item(i);
-            if (!item) throw new Error("Encountered a selected item with no id??");
+    function handleChange(id: string, value: boolean) {
+        const selected = [...selectedAllergies!];
+        const index = selected.findIndex(t => t === id);
+        
+        if (value) {
+            // Value is now TRUE, therefore this allergy is selected and should be added.
+            if (index !== -1) {
+                setError(DBError.custom(`Invariant state. Expected ${id} not to exist in selected. It was present at index ${index}`));
+                return;
+            }
 
-            selected.push(item.value);
+            selected.push(id);
+        } else {
+            // Value is now FALSE, therefore this allergy is NOT selected and must be removed.
+            if (index === -1) {
+                setError(DBError.custom(`Invariant state. Expected ${id} to exist in selected. It did not.`));
+                return;
+            }
+
+            selected.splice(index, 1);
         }
 
         event.setAllergyLabels(selected);
         setSelected(selected);
     }
 
-    return <div className="flex flex-col">
-        <select
-            multiple
-            className="bg-white p-1 shadow-sm rounded-sm"
-            onChange={handleChange}
-        >
-            {
-                allergyOptions.map((o, i) => {
-                    const isSelected = selectedAllergies.find(x => x === o) !== undefined;  
-                    return <option key={i} value={o} selected={isSelected}>{o[0] + o.slice(1).toLowerCase()}</option>
-                })
-            }
-        </select>
-        <p className="text-sm text-center">*Hold down the Ctrl (Windows) or Command (Mac) button to select multiple options.</p>
-        <p className="text-sm text-center">**Ability to add new allergens is coming soon. I can add them for you if you text me.</p>
+    return <div className="flex flex-wrap p-2 bg-white shadow-sm rounded-sm gap-2">
+        { allergyOptions.map((v, i) => {
+            const isSelected = selectedAllergies.find(x => x === v) !== undefined;  
+            return <SelectComponent key={i} id={v} displayText={v[0] + v.slice(1).toLowerCase()} startValue={isSelected} callback={handleChange}/>;
+        })}
     </div> 
 }
