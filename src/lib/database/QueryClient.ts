377a22591e5acf60958bd49911b59ebe14da4156
Michael SB <mjswetnamburla@wm.edu>
2026-09-18 11:52:24 -0400

    Update cache time to actually be 10m

--- src/lib/database/QueryClient.ts
+++ src/lib/database/QueryClient.ts
@@ -3,7 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export default new QueryClient({
     defaultOptions: {
         queries: {
-            staleTime: 10 * 60 * 60 // 10 Minutes
+            staleTime: 10 * 60 * 1000 // 10 Minutes
         }
     }
 });
