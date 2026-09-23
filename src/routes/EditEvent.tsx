2ee956ea974944a3afeab28a20d7ccad9784ea39
Michael SB <mjswetnamburla@wm.edu>
2026-09-23 10:56:09 -0400

    Componentize forms

--- src/routes/EditEvent.tsx
+++ src/routes/EditEvent.tsx
@@ -1,19 +1,61 @@
 import { useForm } from "@tanstack/react-form"
-import type { AnyFieldApi, FieldApi } from "@tanstack/react-form";
+import type { AnyFieldApi } from "@tanstack/react-form";
 
-export function FieldInfo({ field }: { field: AnyFieldApi }) {
-  return (
-    <>
-      {field.state.meta.isTouched && !field.state.meta.isValid ? (
-        <em className="text-red-500 font-semibold">{field.state.meta.errors.join(',')}</em>
-      ) : null}
-      {field.state.meta.isValidating ? 'Validating...' : null}
-    </>
-  )
+type ValidatorProp = { value: string };
+type ValidatorFn = (_: ValidatorProp) => string | undefined;
+
+const onChangeComp = (fn: ValidatorFn) => ({ onChange: (prop: ValidatorProp) => fn(prop) });
+const Validators = {
+     required: () => onChangeComp(({value}) => !value ? 'This field is required.' : undefined ),
+     lengthGreaterEqual: (x: number) => onChangeComp(({value}) => value && (value.length <= x ? `This field must contain ${x} or more characters.` : undefined ) ),
+     lengthGreater: (x: number) => Validators.lengthGreaterEqual(x+1),
+
+     // For now, only onChange is supported. 
+     compose(fns: { onChange: ValidatorFn }[]) {
+	  return {
+	       onChange: (prop: ValidatorProp) => {
+		    for (const fn of fns) {
+			 let result: string | undefined;
+			 if (result = fn.onChange(prop)) return result;
+		    }
+		    return undefined;
+	       }
+	  }
+     }
+};
+
+function InputWrapper({ field, children }: { field: AnyFieldApi, children: React.ReactNode }) {
+     let capitalized: string;
+     if (field.name.length <= 1)
+	  capitalized = field.name;
+     else
+	  capitalized = field.name[0].toUpperCase() + field.name.slice(1)
+
+     return <div className="w-full flex flex-col gap-3 md:grid grid-cols-[1fr_4fr] md:items-center lg:w-3/4 text-center">
+	  <label className="font-semibold text-gray-500 text-right">{capitalized}</label> 
+	  {children}
+	  {field.state.meta.isTouched && !field.state.meta.isValid ? (
+	  <em className="text-red-500 font-semibold col-span-full">{field.state.meta.errors.join(',')}</em>
+	  ) : null}
+	  {field.state.meta.isValidating ? 'Validating...' : null}
+     </div>
 }
 
-function Label({ content }: { content: string }) {
-     return <label className="font-semibold text-gray-500">{content}</label> 
+function TextInput({ field }: { field: AnyFieldApi }) {
+     return <InputWrapper field={field}> 
+	  <input 
+	       className="bg-white p-1 rounded-sm shadow-sm w-full text-center"
+	       id={field.name}
+	       name={field.name}
+	       value={field.state.value}
+	       onBlur={field.handleBlur}
+	       onChange={e => field.handleChange(e.target.value)}
+	  />
+     </InputWrapper>
+}
+
+const Components = {
+     TextInput: (field: AnyFieldApi) => <TextInput field={field} />
 }
 
 export default function EditEvent() {
@@ -25,36 +67,23 @@ export default function EditEvent() {
 	  onSubmit: async ({ value }) => alert(JSON.stringify(value))
      });
      
-     return <div className="flex flex-col items-center">
-	  <form
-	       className="w-full flex flex-col gap-3 md:grid md:grid-cols-[1fr_4fr] md:items-center lg:w-3/4"
-	       onSubmit={e => {
-		    e.preventDefault();
-		    e.stopPropagation();
-		    form.handleSubmit();
-	       }}
-	  >
-	       <form.Field
-		    name="title"
-		    validators={{
-			 onChange: ({ value }) => !value ? 'A title is required' : undefined
-		    }}
-		    children = {field => {
-			 return <>
-			      <Label content="Title:" />
-			      <input 
-				   className="bg-white p-1 rounded-sm shadow-sm w-full text-center"
-				   id={field.name}
-				   name={field.name}
-				   value={field.state.value}
-				   onBlur={field.handleBlur}
-				   onChange={e => field.handleChange(e.target.value)}
-			      />
-			      <FieldInfo field={field} />
-			 </>
-		    }}
-	       />
-	  </form>
-     </div>
-
+     return <form
+	  className="w-full flex flex-col items-center gap-3"
+	  onSubmit={e => {
+	       e.preventDefault();
+	       e.stopPropagation();
+	       form.handleSubmit();
+	  }}
+     >
+	  <form.Field
+	       name="title"
+	       validators={Validators.required()}
+	       children = {Components.TextInput} 
+	  />
+	  <form.Field
+	       name="location"
+	       validators={Validators.compose([Validators.required(), Validators.lengthGreaterEqual(3)])}
+	       children = {Components.TextInput} 
+	  />
+     </form>
 }
